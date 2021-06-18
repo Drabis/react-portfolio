14d2f6ee3b1a4d3c27abcc25a7dbb3dfbb5e0c42
Drabis <drabis47@gmail.com>
2021-06-18 09:51:19 -0400

    projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,50 @@
 import React from "react";
+import projectInfo from "../../assets/";
 
 function Projects() {
   return (
-      // will return the JSX
     <main className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-sm-8 col-md-10">
+          <div className="card mt-5">
+            <article className="card-body">
+              <h1 className="card-title">Projects</h1>
+              <hr />
 
+              <div className="row row-cols-1 row-cols-md-3 content portfolio">
+                {projectInfo.map((project) => (
+                  <div
+                    className="col col-12 col-md-6 col-xl-4 mb-4"
+                    key={project.title}
+                  >
+                    <div className="card h-100">
+                      <img
+                        src={project.image}
+                        className="card-img-top"
+                        alt={project.title}
+                      />
+                      <div className="card-body proj-description">
+                        <h5 className="card-title">{project.title}</h5>
+                        <p className="card-text">{project.description}</p>
+                      </div>
+                      <div className="card-body proj-links">
+                        <a href={project.repo} className="card-link">
+                          Repo
+                        </a>
+                        <a href={project.url} className="card-link">
+                          Deployed Site
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
+}
 
-}  
-export default Projects; 
+export default Projects;
