beb9677842c085d695379a2a093a332750efe4b8
Drabis <drabis47@gmail.com>
2021-06-18 09:39:32 -0400

    about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,38 @@
-import React from 'react';
-// Will import profile photo
+import React from "react";
+import { profile } from "../../assets";
 
 function About() {
-    return(
-        // the body of the JSX
-        <main className='conatainer'>
+  return (
+    <main className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-10 col-lg-8 col-xl-7">
+          <div className="card mt-5">
+            <article className="card-body">
+              <h1 className="card-title">About Me</h1>
+              <hr />
+              <div className="content about-me">
+                <div>
+                  <img src={profile} alt="My profile" />
+                </div>
+                <p className="card-text">
+                   Hey all! I'm Aboulaye Drabo from the Ivory Coast where
+                  the music is always great and the food is always good, I'm a
+                  native french speaker. I am a full-stack web developer with a
+                  focus on the Front End. I'm excited to utilize technology to
+                  foster growth, development, and progress. I'm passionate about
+                  helping others and I believe technology has a great impact on
+                  everyday lives. <br />
+                  Skills: HTML3, CSS3, jQuery, Gatsby, Bootstrap, , JavaScript,
+                  Express.js, Handlebars, Node.js, MongoDB, MySQL, React.js and
+                  API Integration
+                </p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
 
-        </main>
-
-    )
-};
 export default About;
