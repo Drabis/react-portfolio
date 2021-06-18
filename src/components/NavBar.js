6c021a81ea6256fd2585fc0ad1347eca767c0d2f
Drabis <drabis47@gmail.com>
2021-06-18 09:59:02 -0400

    navbar adde

---
+++ src/components/NavBar.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar() {
+  // We'll go into the Hooks API later, for now, we are just using some code
+  // from the react-router docs (https://reacttraining.com/react-router/web/api/Hooks/uselocation)
+  // This allows the component to check the route any time the user uses a link to navigate.
+  const location = useLocation();
+
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="col-8">
+          <Link to="/" className="navbar-brand">
+            <span>Aboulaye Drabo</span>
+          </Link>
+        </div>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Nav Items */}
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={
+                  location.pathname === "/" ? "nav-link active" : "nav-link"
+                }
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/projects"
+                className={
+                  location.pathname === "/projects"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className={
+                  location.pathname === "/contact"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
