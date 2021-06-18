719235dc347e17d5cfaa0950231aa910ad6a3d3b
Drabis <drabis47@gmail.com>
2021-06-18 09:54:05 -0400

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 fixed-bottom">
+      <div className="container">
+        <span>&copy; Aboulaye Drabo 2021</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
