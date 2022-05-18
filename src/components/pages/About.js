93212a428450c72700ae666e8f4e7ce96f94d3df
Aboulaye Drabo <drabis47@gmail.com>
2022-05-17 20:26:01 -0500

    re-phrase

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,13 +16,8 @@ function About() {
                   {/* <img src={process.env.PUBLIC_URL + '/assets/images/abou.jpg'} alt="" /> */}
                 </div>
                 <p className="card-text">
-                   Hey all! I'm Aboulaye Drabo from the Ivory Coast where
-                  the music is always great and the food is always good, I'm a
-                  native french speaker. I graduated from Columbia University Coding Bootcamp where I learned to build a responsive full-stack web Applications using the lastest technology.
-                   I'm excited to utilize technology to
-                  foster growth, development, and progress in this new world. I'm passionate about
-                  helping others and I believe technology has a great impact on our
-                  everyday lives. <br />
+                Hey! 
+                  I'm Aboulaye Drabo from the Ivory Coast, where the music is always great and the food is always good. I have been living in NYC for about eight years now. I'm a native French speaker. I graduated from Columbia University Coding Bootcamp, where I learned to build responsive full-stack web Applications using the latest technology. I'm excited to utilize technology to foster growth, development, and progress in this new world. I'm passionate about helping others, and I also believe that technology has a great way of impacting our everyday lives. <br />
                   <b>Skills:</b> 
                   HTML5, CSS, JavaScript, Java
                   Express.js, MongoDB, MySQL, Spring framworks, React.js, React Native and
