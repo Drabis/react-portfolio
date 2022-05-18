73becde59996bd7403665974a3052b1601a26c00
Aboulaye Drabo <drabis47@gmail.com>
2022-05-18 17:03:58 -0500

    update resume link

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -35,7 +35,7 @@ function Contact(props) {
                 </a>
 
                 <a
-                  href="https://docs.google.com/document/d/1zsHKYeWYKsRIlyX2ds-EDj7v2S5V9IhezjHxCoM3q74/edit?usp=sharing"
+                  href="https://docs.google.com/document/d/1E0FuXE-48IiL8_NC62ajcYEcnePsjuCCxnmjwVDxCOA/edit"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="list-group-item list-group-item-action"
